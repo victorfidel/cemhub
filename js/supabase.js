d390862f14131b53b78d044eeb103170fbc73e13
victorfidel <126929999+victorfidel@users.noreply.github.com>
2026-05-21 17:02:57 +0100

    Update Supabase URL and anon key

--- js/supabase.js
+++ js/supabase.js
@@ -2,8 +2,8 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.45.0/+esm'
 
 // ⚠️ PASTE YOUR ACTUAL VALUES HERE from Step 2
-const SUPABASE_URL = 'https://fveuxzijvutieavzdfvg.supabase.co' 
-const SUPABASE_ANON_KEY = 'eyJhbGci...paste_your_full_anon_key_here'
+const SUPABASE_URL = 'https://srzhahwfvitirabhsjsw.supabase.co' 
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyemhhaHdmdml0aXJhYmhzanN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzNzMzOTEsImV4cCI6MjA5NDk0OTM5MX0.0BwJcguv0KSzSFVswN3JnfOOvtnALF1cVP8xW3cWnvs'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
